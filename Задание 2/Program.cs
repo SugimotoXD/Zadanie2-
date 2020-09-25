789cea4d4072c63172202ff847e4a2059f1334ec
userdb10 <userdb10@mail.ru>
2020-09-25 15:31:21 +0200

    Добавьте файлы проекта.

---
+++ Задание 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double I, R, r;
+                Console.Write("Enter R");
+                R = double.Parse(Console.ReadLine());
+                Console.Write("Enter r");
+                r = double.Parse(Console.ReadLine());
+                I = 285 / (R + r * r);
+                {
+                    Console.WriteLine("I=" + I);
+                }
+            }
+            catch (Exception)
+
+            {
+
+
+
+
+
+
+            }
+        }
+    }
+}
